2c1c6b86d67d05b1622aa04d7077c25c5115bc27
LC01 <LC01@LC01-PC>
2018-01-17 16:17:54 +0800

    Progress 17 Jan 2018

---
+++ src/com/devproject/alphabet/Alphabet.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.devproject.alphabet;
+
+import com.devproject.form.Main;
+
+/**
+ *
+ * @author LC01
+ */
+public class Alphabet {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Main FormUtama = new Main();
+        FormUtama.setVisible(true);
+    }
+    
+}
